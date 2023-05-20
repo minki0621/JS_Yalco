14d1b52e43abcaa56978c2f2d2558ae5ac31aa11
Raphael.Shin <108261108+shinseogyong@users.noreply.github.com>
2023-05-20 18:21:06 +0900

    Add files via upload
    05.20 test upload

---
+++ practice_yalco.js
@@ -0,0 +1,26 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// ⚠️ 변수(i)를 사용하므로 위험요소 존재
+for (let i = 0; i < numbers.length; i++) {
+  // 이곳에 i를 변경하는 코드가 들어간다면...
+  console.log(numbers[i]);
+}
+
+// ⭐️ 변수를 사용하지 않음으로 보다 안전
+for (const num of numbers) {
+  console.log(num);
+}
+
+const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers2 = [];
+
+for (let num of numbers1) {
+  num++; // ⚠️ 복사된 값. let 사용 주목
+  numbers2.push(num + 1);
+}
+console.log(numbers1, numbers2);
+
+for (let i = 0; i < numbers1.length; i++) {
+  numbers1[i]++; // ⚠️ 실제 값에 인덱스로 접근 - 원본 배열의 내용 변경
+}
+console.log(numbers1, numbers2);
