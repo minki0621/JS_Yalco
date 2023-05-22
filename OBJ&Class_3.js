f08f327323cf44613a19bdaeed1e08d0f0f7ca7f
shinseogyong <108261108+shinseogyong@users.noreply.github.com>
2023-05-22 15:20:12 +0900

    Update OBJ&Class_3.js

--- OBJ&Class_3.js
+++ OBJ&Class_3.js
@@ -1,16 +1,16 @@
 class Family {
-  constructor (name, guanxi, married) { //name과 guanxi 매개변수를 받아 객체의 속성을 초기화
+  constructor(name, guanxi, married) {
     this.name = name;
     this.guanxi = guanxi;
     this.married = married;
   }
-  introduce () {
+  introduce() {
     return `이름은 ${this.name}이고 ${this.guanxi}역학을 맡고 있습니다.
-    현재 ${this.merried ? '기혼' : '미혼'}입니다.`;
+    현재 ${this.married ? '기혼' : '미혼'}입니다.`;
   }
 }
 
 const father = new Family('신동석', '아버지', true);
-const son = new Family('신민기' ,'아들', false);
+const son = new Family('신민기', '아들', false);
 console.log(father.introduce());
 console.log(son.introduce());
