67aad7d1f57e6eb636af57c635731456b23ac11c
shinseogyong <108261108+shinseogyong@users.noreply.github.com>
2023-05-22 15:52:52 +0900

    클래스와 constructor
    constructor 메서드는 인자를 받아서
    인스턴스 생성시 프로퍼티를 초기화한다

--- OBJ&Class_3.js
+++ OBJ&Class_3.js
@@ -1,16 +1,16 @@
 class Family {
-  constructor(name, guanxi, married) {
+  constructor(name, guanxi, married) {  //💡name과 guanxi 매개변수를 받아 객체의 속성을 초기화
     this.name = name;
     this.guanxi = guanxi;
     this.married = married;
   }
-  introduce() {
+  introduce() {  //💡 메서드
     return `이름은 ${this.name}이고 ${this.guanxi}역학을 맡고 있습니다.
     현재 ${this.married ? '기혼' : '미혼'}입니다.`;
   }
 }
 
 const father = new Family('신동석', '아버지', true);
-const son = new Family('신민기', '아들', false);
+const son = new Family('신민기', '아들'); //적지 않으면 기본적으로 false 값 할당
 console.log(father.introduce());
 console.log(son.introduce());
